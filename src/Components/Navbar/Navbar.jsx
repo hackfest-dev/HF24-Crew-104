840d2bd11c36863b512238548d0dbba88f3aa106
Chinmayee Patankar <chinmayeep2753@gmail.com>
2024-04-05 22:48:04 +0530

    Signup Form

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,35 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import './Navbar.css'
-import { Link } from 'react-scroll';
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+import { Link } from "react-scroll";
 
-
-
-  
 const Navbar = () => {
- const[sticky,setSticky]=useState(false);
- useEffect(()=>{
-  window.addEventListener('scroll',()=>{
-    window.scrollY>50?setSticky(true):setSticky(false);
-  })
- },[])
- const handleLoginButtonClick = () => {
-  window.location.href = '/register';
-};
+  const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+  const handleLoginButtonClick = () => {
+    window.location.href = "/register";
+  };
 
   return (
-    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-
-     <img  alt='' className='logo'/>
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+      <img alt="" className="logo" />
       <ul>
-        <li><Link to='hero' smooth={true} offset={0} duration={500}><strong>Home</strong></Link></li>
-        <li><Link to='abouts' smooth={true} offset={0} duration={500}><strong>About</strong></Link></li>
-        <li><Link to='footer' smooth={true} offset={0} duration={500}><strong>Contact Us</strong></Link></li>
-        <li><button className='btn' onClick={handleLoginButtonClick}><strong>Login</strong></button></li>
-
+        <li>
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            <strong>Home</strong>
+          </Link>
+        </li>
+        <li>
+          <Link to="abouts" smooth={true} offset={0} duration={500}>
+            <strong>About</strong>
+          </Link>
+        </li>
+        <li>
+          <Link to="footer" smooth={true} offset={0} duration={500}>
+            <strong>Contact Us</strong>
+          </Link>
+        </li>
+        <li>
+          <button className="btn" onClick={handleLoginButtonClick}>
+            <strong>Login</strong>
+          </button>
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-
-export default Navbar
+export default Navbar;
